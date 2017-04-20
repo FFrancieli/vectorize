6c8a158be84f2eb32cecc2171a6b3ddc89be4a6e
ffrancieli <francielifrv@gmail.com>
2017-04-19 22:15:41 -0300

    first commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject vectorize "0.1.0-SNAPSHOT"
+  :description "small lib for vector operations"
+  :url "http://example.com/FIXME"
+    :dependencies [[org.clojure/clojure "1.8.0"]
+                [midje "1.8.3"]]
+  :plugins [[lein-midje "3.2.1"]])
